04507135d82804124bd1eb898b61a6afd3973b3b
b14z3 <stainedhat@gmail.com>
2014-05-06 00:47:27 -0700

    Start of a multi filetype cracking program. Supports pdf and zip right now.

--- cracker.py
+++ cracker.py
@@ -2,39 +2,55 @@
 
 import zipfile
 import argparse
+import pyPdf  
 from threading import Thread
 
 
 def extractZip(zip, password):
     try:
-        zip.extractall(pwd=password)
+        zfile = zipfile.ZipFile(zip)
+        zfile.extractall(pwd=password)
         print '[+] Found password ' + password + '\n'
     except:
         pass
+
+def openPdf(pdfile, password):
+    try:
+        pdf = pyPdf.PdfFileReader(open(pdf))
+        pdf.decrypt(password)  
+    except:
+        pass
     
-def crackZip(zfile, dfile):
-    zip = zipfile.ZipFile(zfile)
-    dict = open(dfile, "r")
-    for line in dict.readlines():
+def crackMain(function, file, dict):
+    dictfile = open(dict, "r")
+    for line in dictfile.readlines():
         password = line.strip("\n")
-        t = Thread(target=extractZip, args=(zip, password))
-        t.start()
-    
+        t = Thread(target=function, args=(file, password))
+        t.start() 
+   
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', dest='file', help='The name of the file to crack')
     parser.add_argument('-d', '--dict', dest='dict', help='A dictionary to use for cracking')
-    parser.add_argument('-z', '--zip', dest='zip', help='Crack a Zip file')    
+    parser.add_argument('-z', '--zip', dest='zip', help='Crack a Zip file')
+    parser.add_argument('-p', '--pdf', dest='pdf', help='Crack a PDF file') 
     args = parser.parse_args()
     if args.file is None or args.dict is None:
         print parser.usage
+        print "\n [!] You must specify a file to crack and a dictionary! \n"
+        exit(0)
+    elif not args.zip or not args.pdf:
+        print parser.usage
+        print "\n [!] Please specify the file type to crack! \n"
         exit(0)
     else:
         crackfile = args.file
         dfile = args.dict
         print dfile, crackfile
     if args.zip:
-        crackZip(crackfile, dfile)
+        crackMain('extractZip', crackfile, dfile)
+    elif args.pdf:
+        crackMain('openPdf', crackfile, dfile)
 
 if __name__ == '__main__':
     main()
