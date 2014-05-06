ecdabd6902707c62301e6c4b6a9e4e7f771e5c12
b14z3 <stainedhat@gmail.com>
2014-05-05 23:59:34 -0700

    Start of a multi filetype cracking program

---
+++ cracker.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+import zipfile
+import argparse
+from threading import Thread
+
+
+def extractZip(zip, password):
+    try:
+        zip.extractall(pwd=password)
+        print '[+] Found password ' + password + '\n'
+    except:
+        pass
+    
+def crackZip(zfile, dfile):
+    zip = zipfile.ZipFile(zfile)
+    dict = open(dfile, "r")
+    for line in dict.readlines():
+        password = line.strip("\n")
+        t = Thread(target=extractZip, args=(zip, password))
+        t.start()
+    
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--file', dest='file', help='The name of the file to crack')
+    parser.add_argument('-d', '--dict', dest='dict', help='A dictionary to use for cracking')
+    parser.add_argument('-z', '--zip', dest='dict', help='Crack a Zip file')    
+    args = parser.parse_args()
+    if args.file is None or args.dict is None:
+        print parser.usage
+        exit(0)
+    else:
+        crackfile = args.file
+        dfile = args.dict
+        print dfile, crackfile
+
+if __name__ == '__main__':
+    main()
