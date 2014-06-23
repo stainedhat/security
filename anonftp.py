8c8150181c9cad379651759c649b792b70f00150
b14z3 <stainedhat@gmail.com>
2014-06-23 00:12:38 -0700

    Started adding brute force section but far from complete. Still need to figure out threading when single ip vs multiple and single user vs multiple. Brute needs lots of testing but anon works

--- anonftp.py
+++ anonftp.py
@@ -23,8 +23,25 @@ def anonLogin(ip, timeout):
         else:
             pass
         #print ip, e
+        
+def bruteLogin(ip, user, pw, timeout):
+    try:
+        ftp = ftplib.FTP()
+        ftp.connect(ip, 21, timeout)
+        email = binascii.b2a_hex(os.urandom(3)) + '@' + binascii.b2a_hex(os.urandom(3)) + '.com'
+        ftp.login('anonymous', email)
+        print "[*] " + str(ip) + ' : Anonymous FTP login succeeded!'
+        success = ip + "\n"
+        ftp.quit()
+        return success
+    except Exception, e:
+        if verbose:
+            print ip, e
+        else:
+            pass
+        #print ip, e
 
-def worker(ip_queue, found_queue, timeout):
+def anon_worker(ip_queue, found_queue, timeout):
     try:
         for ip in iter(ip_queue.get, 'STOP'):
             result = anonLogin(ip, timeout)
@@ -32,9 +49,20 @@ def worker(ip_queue, found_queue, timeout):
                 found_queue.put(result)
     except:
         pass
+    
+def brute_worker(ip_queue, user_queue, pass_queue, found_queue, timeout):
+    try:
+        for ip in iter(ip_queue.get, 'STOP'):
+            for user in iter(user_queue.get, 'STOP'):
+                for pw in iter(pass_queue.get, 'STOP'):
+                    result = bruteLogin(ip, user, pw, timeout)
+                    if result:
+                        found_queue.put(result)
+    except:
+        pass
 
-#called when a list is provided then iterates through the list and call anonLogin()
-def hitlist(list, timeout, threads):
+#called when a anon is provided with a list then iterates through the list and calls anonLogin()
+def anonlist(list, timeout, threads):
     #setup queues and threads
     ip_queue = Queue()
     found_queue = Queue()
@@ -54,7 +82,7 @@ def hitlist(list, timeout, threads):
     
     #start threads
     for x in xrange(threads):
-        p = Process(target=worker, args=(ip_queue, found_queue, timeout))
+        p = Process(target=anon_worker, args=(ip_queue, found_queue, timeout))
         p.start()
         processes.append(p)
         ip_queue.put('STOP')
@@ -70,17 +98,97 @@ def hitlist(list, timeout, threads):
         for found in iter(found_queue.get, 'STOP'):
             ofile.write(found)
         ofile.close()
+
+#called when a brute force is specified and brute forces login
+def brutelist(iplist, passlist, userlist, timeout, threads):
+    #setup queues and threads
+    ip_queue = Queue()
+    pass_queue = Queue()
+    user_queue = Queue()
+    found_queue = Queue()
+    global processes
+    processes = []
+    
+    #build ip_queue
+    if os.path.isfile(iplist):
+        f = open(iplist, 'r')
+        for line in f.readlines():
+            line = line.strip('\n').strip('\r')
+            ip_queue.put(line)
+        f.close()
+    else: 
+        print "File'%s' not found. Check path and try again!" % iplist
+        exit(0)
         
+    #build pass_queue
+    if os.path.isfile(passlist):
+        f = open(passlist, 'r')
+        for line in f.readlines():
+            line = line.strip('\n').strip('\r')
+            pass_queue.put(line)
+        f.close()
+    else: 
+        print "List '%s' not found. Check path and try again!" % passlist
+        exit(0)
+    
+    #build user_queue
+    if os.path.isfile(userlist):
+        f = open(userlist, 'r')
+        for line in f.readlines():
+            line = line.strip('\n').strip('\r')
+            user_queue.put(line)
+        f.close()
+    else: 
+        print "List '%s' not found. Check path and try again!" % userlist
+        exit(0)
+    
+    #start threads
+    for x in xrange(threads):
+        p = Process(target=brute_worker, args=(ip_queue, user_queue, pass_queue, found_queue, timeout))
+        p.start()
+        processes.append(p)
+        ip_queue.put('STOP')
+        user_queue.put('STOP')
+        pass_queue.put('STOP')
+    
+    #wait for processes to complete and put end tag into found_queue
+    for p in processes:
+        p.join()
+    found_queue.put('STOP')
+    
+    #create output file
+    if outfile:
+        ofile = open(outfile, 'a')
+        for found in iter(found_queue.get, 'STOP'):
+            ofile.write(found)
+        ofile.close()
 
 def main():
     #setup args
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--ip', help="Check a single target IP")
-    parser.add_argument('-l', '--list', help="Check all targets in a list. List should be one target IP per line")
-    parser.add_argument('-o', '--outfile', help="Output found targets to a file")
-    parser.add_argument('-t', '--threads', default=5, type=int, help="Number of threads to use for scanning [default: 5]")
-    parser.add_argument('-n', '--timeout', default=10, type=int, help="Check a single target IP [default: 10]")
-    parser.add_argument('-v', '--verbose', action='store_true', help="Verbose output which includes errors and failure messages")
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, epilog='''
+Examples:
+
+Anonymous login testing: %(prog)s -a -t 10 -n 5 -l ip_list.txt -o results.txt
+    ^Checks for anonymous login for each IP in ip_list.txt using 10 threads and a timeout of 5 seconds.\n
+Brute Force login: %(prog)s -b -t 10 -i 192.168.0.100 -u admin -p pass_list.txt -o output.txt
+    ^Brute forces ftp login for admin user on 192.168.0.100 using 10 threads and pass_list.txt
+        ''', conflict_handler='resolve')
+    required = parser.add_argument_group("Required", "An attack method and IP or list of IP's is required")
+    brute = parser.add_argument_group("Brute Force", "Options for FTP brute force testing")
+    general = parser.add_argument_group("General options", "Fine tune timing, threads, and output")
+    required.add_argument('-i', '--ip', help="Check a single target IP")
+    required.add_argument('-l', '--list', help="Check all targets in a list. List should be one target IP per line")
+    general.add_argument('-o', '--outfile', help="Output found targets to a file")
+    general.add_argument('-t', '--threads', default=5, type=int, help="Number of threads to use for scanning [default: 5]")
+    general.add_argument('-n', '--timeout', default=10, type=int, help="Check a single target IP [default: 10]")
+    general.add_argument('-v', '--verbose', action='store_true', help="Verbose output which includes errors and failure messages")
+    general.add_argument('-h', '--help', action='store_true', help="Shows options and help menu")
+    required.add_argument('-a', '--anon', action='store_true', help="Check for anonymous FTP login")
+    required.add_argument('-b', '--brute', action='store_true', help="Brute force FTP login credentials")
+    brute.add_argument('-u', '--username', help="Username to use for brute force login")
+    brute.add_argument('-U', '--user-list', dest='userlist', help="Username to use for brute force login")
+    brute.add_argument('-p', '--pass-list', dest='passlist', help="Password list to use for brute force login")
+    #assign args to variables
     args = parser.parse_args()
     ip = args.ip
     list = args.list
@@ -90,8 +198,11 @@ def main():
     global verbose
     outfile = args.outfile
     verbose = args.verbose
+    username = args.username
+    userlist = args.userlist
+    passlist = args.passlist
     if ip:
-        verbose = True #set this so that a single IP scan has an output
+        verbose = True #set this so that a single IP scan has an output to stdout
     
     #check if outfile exists and prompt for overwrite if so
     if outfile:
@@ -104,21 +215,45 @@ def main():
                 print "[!] Exiting now!"
                 exit()
     
-    #check to make sure user has specified target(s)
-    if ip == None and list == None:
+    #check to make sure user has specified target(s) and attack type
+    if (ip == None and list == None) or (not args.anon and not args.brute):
         parser.print_help()
-        print "\n[!] You must specify either a single target or a list of targets\n"
         exit(0)
     
     #check args and decide whether to handle a list or single ip then 
     #actually start the login attempts
-    print "[!] Testing Anonymous FTP Login.."
-    if list: hitlist(list, timeout, threads)
-    if ip: anonLogin(ip, timeout)
-    
+    if args.anon:
+        print "[!] Testing Anonymous FTP Login.."
+        if list: anonlist(list, timeout, threads)
+        if ip: anonLogin(ip, timeout)
+    if args.brute:
+        if (not userlist or not username) or not passlist:
+            print parser.print_help()
+            exit(0)
+        if username:
+            f = open('/tmp/ftpuser.tmp', 'w')
+            f.write(username)
+            f.close()
+            userlist = '/tmp/ftpuser.tmp'
+        if ip:
+            f = open('/tmp/ftpip.tmp', 'w')
+            f.write(ip)
+            f.close()
+            iplist = '/tmp/ftpip.tmp'
+        print "[!] Brute forcing FTP Login.."
+        brutelist(iplist, passlist, userlist, timeout, threads)
+        
+def cleanup():
+    if os.path.isfile('/tmp/ftpuser.tmp'):
+        os.remove('/tmp/ftpuser.tmp')
+    if os.path.isfile('/tmp/ftpip.tmp'):
+        os.remove('/tmp/ftpip.tmp')
+           
 if __name__ == '__main__':
     try:
         main()
+        cleanup()
     except KeyboardInterrupt:
+        cleanup()
         print "\nKeyboard interrupt caught! Exiting now....."
         exit(0)
