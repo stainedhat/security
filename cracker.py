e12550d0c8e2fedb146fb7ccfc1a587910e23947
b14z3 <stainedhat@gmail.com>
2014-07-15 22:22:31 -0700

    Fixed file open issue when too many passwords are checked by adding zfile.close inside the loop fo each call

--- cracker.py
+++ cracker.py
@@ -10,8 +10,9 @@ def extractZip(zip, password):
         zfile = zipfile.ZipFile(zip)
         zfile.extractall(pwd=password)
         print '[+] Found password: ' + password + '\n'
+        zfile.close()
     except:
-        pass
+        zfile.close()
 
 def openPdf(pdfile, password):
     try:
@@ -25,15 +26,18 @@ def crackMain(function, file, dict):
     dictfile = open(dict, "r")
     for line in dictfile.readlines():
         password = line.strip("\n")
-        t = Thread(target=function, args=(file, password))
+        if function == "extractZip":
+            t = Thread(target=extractZip, args=(file, password))
+        if function == "openPdf":
+            t = Thread(target=openPdf, args=(file, password))
         t.start() 
    
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', '--file', dest='file', required=True, help='The name of the file to crack (Required)')
     parser.add_argument('-d', '--dict', dest='dict', required=True, help='A dictionary to use for cracking (Required)')
-    parser.add_argument('-z', '--zip', dest='zip', help='Crack a Zip file')
-    parser.add_argument('-p', '--pdf', dest='pdf', help='Crack a PDF file') 
+    parser.add_argument('-z', '--zip', action="store_true", dest='zip', help='Crack a Zip file')
+    parser.add_argument('-p', '--pdf', action="store_true", dest='pdf', help='Crack a PDF file')
     args = parser.parse_args()
     if args.file is None or args.dict is None:
         print parser.usage
